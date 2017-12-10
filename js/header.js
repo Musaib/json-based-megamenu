0c25585a4dec3595177fe3d4d8f46685bcbbabdc
Musaib <musaib1.mukri@gmail.com>
2017-12-10 16:05:05 +0530

    set up

---
+++ js/header.js
@@ -0,0 +1,8 @@
+//Mobile Menu Initialised
+$(document).ready(function() {
+    $('#simple-menu').sidr({
+        side: 'right',
+        name: 'header-nav',
+        source: '#navigation' 
+    });
+}); 
