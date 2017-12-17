1c08e3dce603b4faae8e644986fc4fb026619357
Musaib <musaib1.mukri@gmail.com>
2017-12-17 17:02:36 +0530

    final changes

--- js/header.js
+++ js/header.js
@@ -1,8 +1,9 @@
-//Mobile Menu Initialised
-$(document).ready(function() {
-    $('#simple-menu').sidr({
-        side: 'right',
-        name: 'header-nav',
-        source: '#navigation' 
-    });
-}); 
+var api = 'https://raw.githubusercontent.com/Musaib/responsigation/master/navigation.json';
+
+$.getJSON(api, getData)
+
+function getData(data){
+    var main = Handlebars.compile( $( "#header" ).html() );
+    Handlebars.registerPartial( "list", $( "#list" ).html() );
+    $( "#nav" ).html( main({ items: data.items }) );
+}
