bcdb2933e3603142b48370a122a33d981081fa38
Musaib <musaib1.mukri@gmail.com>
2017-12-21 12:17:40 +0530

    Added mustache template

--- js/header.js
+++ js/header.js
@@ -1,9 +1,1 @@
-var api = 'https://raw.githubusercontent.com/Musaib/responsigation/master/navigation.json';
 
-$.getJSON(api, getData)
-
-function getData(data){
-    var main = Handlebars.compile( $( "#header" ).html() );
-    Handlebars.registerPartial( "list", $( "#list" ).html() );
-    $( "#nav" ).html( main({ items: data.items }) );
-}
